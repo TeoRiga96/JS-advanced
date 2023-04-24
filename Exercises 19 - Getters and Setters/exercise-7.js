4987203cb0afde3256b6a618134ef435507cc368
TeoRiga96 <rigamonti.teo@gmail.com>
2023-04-24 15:39:24 +0200

    add

--- Exercises 19 - Getters and Setters/exercise-7.js
+++ Exercises 19 - Getters and Setters/exercise-7.js
@@ -1,13 +1,15 @@
 class Person {
-  
+  #firstName = "";
+  #lastName = "";
+  #age = 0;
   constructor(firstName, lastName, age){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
   }
   
   get fullName(){
-    return this.firstName + " " + this.lastName;
+    return this.#firstName + " " + this.#lastName;
   }
    
   
@@ -15,9 +17,9 @@ class Person {
 
 const person = new Person('Mario', 'Rossi', 25);
 console.log(person.fullName);
-// console.log(person.firstName)
+console.log(person.firstName)
 
-person.firstName = 'Maria'; //perchè devo fare un set e get per firstName se già così può essere riassegnato ad un altro valore?? 
-person.lastName = 'Verdi'; //stessa domanda 
-// console.log(person.firstName)
+person.firstName = 'Maria'; 
+person.lastName = 'Verdi'; 
+console.log(person.firstName)
 console.log(person.fullName);
