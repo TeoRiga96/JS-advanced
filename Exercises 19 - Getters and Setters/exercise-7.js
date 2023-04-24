4b9a552231aa1052b0e7f79b11815aec1bd515e8
TeoRiga96 <rigamonti.teo@gmail.com>
2023-04-24 16:54:09 +0200

    quarto esercizio

--- Exercises 19 - Getters and Setters/exercise-7.js
+++ Exercises 19 - Getters and Setters/exercise-7.js
@@ -1,13 +1,24 @@
 class Person {
   #firstName = "";
   #lastName = "";
-  #age = 0;
+  age = 0;
   constructor(firstName, lastName, age){
     this.#firstName = firstName;
     this.#lastName = lastName;
-    this.#age = age;
+    this.age = age;
   }
-  
+  set firstName(newFirstName){
+    this.#firstName = newFirstName;
+  }
+  get firstName(){
+    return this.#firstName;
+  }
+  set lastName(newLastName){
+    this.#lastName = newLastName;
+  }
+  get lastName(){
+    return this.#lastName;
+  }
   get fullName(){
     return this.#firstName + " " + this.#lastName;
   }
@@ -17,9 +28,8 @@ class Person {
 
 const person = new Person('Mario', 'Rossi', 25);
 console.log(person.fullName);
-console.log(person.firstName)
 
 person.firstName = 'Maria'; 
 person.lastName = 'Verdi'; 
-console.log(person.firstName)
+console.log(person.firstName)//prova. in questo caso firstName è il get e non il parametro (perchè è privato)
 console.log(person.fullName);
