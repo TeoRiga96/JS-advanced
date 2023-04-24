dc51eec65b716ec86a8cadb11ea3a56d027c3a0f
TeoRiga96 <rigamonti.teo@gmail.com>
2023-04-24 16:09:18 +0200

    quinto esercizio

--- Exercise 25 - Rest parameters and spread syntax/exercise-3.js
+++ Exercise 25 - Rest parameters and spread syntax/exercise-3.js
@@ -1,4 +1,8 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-numberStore.push(newNumber);
-console.log(numberStore);
+
+// numberStore.push(newNumber);
+// console.log(numberStore);
+
+const newNumberStore = [...numberStore, newNumber];
+console.log(newNumberStore);
