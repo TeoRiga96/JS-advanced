47482c9ae602097dd0acba21cc857604ea5a0e33
TeoRiga96 <rigamonti.teo@gmail.com>
2023-04-25 19:26:45 +0200

    domanda da fare

--- Exercise 31 - JSON Methods/exercise-3.js
+++ Exercise 31 - JSON Methods/exercise-3.js
@@ -12,7 +12,7 @@ const person1 = {
 
 const person12 = JSON.stringify(person1) ;
 const person2 = JSON.parse(person12);
-person2.address.city = 'Milan';
+person2.address.city = 'Milan'; //ma se facciamo una deepcopy con lo spread (...) perchè person2 modifica person1. non è un nuovo oggetto???
 
 console.log(person1);
 console.log(person2);
