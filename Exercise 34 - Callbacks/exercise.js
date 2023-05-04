4f2ac70a1ef316803ad448d5d64b9064a51184a3
TeoRiga96 <rigamonti.teo@gmail.com>
2023-05-04 20:01:04 +0200

    me li stampa su due righe differenti. per farlo stamapre in sequenza come si può fare?

--- Exercise 34 - Callbacks/exercise.js
+++ Exercise 34 - Callbacks/exercise.js
@@ -1,1 +1,7 @@
 
+function printAsyncName(callback, name){
+    setTimeout(callback, 1000);
+    setTimeout(() => console.log(name), 2000);
+}
+
+printAsyncName(() => console.log('Hello'), 'Gianni'); 
