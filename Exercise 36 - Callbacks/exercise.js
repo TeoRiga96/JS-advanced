45c9ac2bde249c8673eef5db3e6a5b326c298290
TeoRiga96 <rigamonti.teo@gmail.com>
2023-05-04 21:51:01 +0200

    terzo esercizio

---
+++ Exercise 36 - Callbacks/exercise.js
@@ -0,0 +1,8 @@
+function repeatHello(callback){
+    let interval = setInterval(callback, 1000);
+    setTimeout(() => clearInterval(interval), 5000);
+}
+//let printHello = () => console.log('Hello')
+//repeatHello(printHello);
+
+repeatHello(() => console.log('Hello'));
