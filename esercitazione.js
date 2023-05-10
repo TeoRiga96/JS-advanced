9424310e8692b2ef7784b0148d148f6797653427
TeoRiga96 <rigamonti.teo@gmail.com>
2023-05-10 09:42:03 +0200

    metodo async e con promise aggiustato

--- esercitazione.js
+++ esercitazione.js
@@ -13,26 +13,28 @@ class Class1 {
         this.students = val;
     }
 
-    async fetchUrl(url){
-       const risp = await fetch(url, {
-        headers: { "X-Api-Key": "rxll5ONMoj4N9oaYkjPRvw==mNIx2udRZoGzoSBg" }})
-        const rispJson = await risp.json();
-        return  this.newStudents = rispJson.username;
-    }
-
-    // fetchUrl(url){
-    //     fetch(url,  {
+    // async fetchUrl(url){
+    //    const risp = await fetch(url, {
     //     headers: { "X-Api-Key": "rxll5ONMoj4N9oaYkjPRvw==mNIx2udRZoGzoSBg" }})
-    //     .then((risp) => risp.json())
-    //     .then((toJson) => (toJson.username))
-    //     .then((username) => (this.newStudents = username))
-    //     .catch((err) => console.log(err))
+    //     const rispJson = await risp.json();
+    //     this.newStudents = await rispJson;
+    //     return  console.log(this.getStudents)
     // }
+
+    fetchUrl(url){
+        fetch(url,  {
+        headers: { "X-Api-Key": "rxll5ONMoj4N9oaYkjPRvw==mNIx2udRZoGzoSBg" }})
+        .then((risp) => risp.json())
+        .then((toJson) => (toJson.username))
+        .then((username) => (this.newStudents = username))
+        .then(() => console.log(this.getStudents))
+        .catch((err) => console.log(err))
+    }
 }
    
-let stud1 = new Class1("")
+let stud1 = new Class1("Paolo")
 stud1.fetchUrl("https://api.api-ninjas.com/v1/randomuser") 
-setTimeout(() => console.log(stud1.getStudents), 3000) 
+ 
 
 
 
